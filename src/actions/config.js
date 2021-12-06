60671e10a5cb463503fc7b6c2f85db737ef7bbdf
FONDIO Kimba <kimba.fondio@data354.co>
2021-12-06 07:00:00 +0000

    set api url to production mode

--- src/actions/config.js
+++ src/actions/config.js
@@ -1,2 +1,2 @@
-export const baseURL = 'http://127.0.0.1:8000'
-// export const baseURL = 'https://clar-cocolife-api.azurewebsites.net'
+// export const baseURL = 'http://127.0.0.1:8000'
+export const baseURL = 'https://clar-cocolife-api.azurewebsites.net'
